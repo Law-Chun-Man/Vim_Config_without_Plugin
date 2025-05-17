d55383a59e6e66fd09da2f23a33b78fbed000e87
Law-Chun-Man <126952896+Law-Chun-Man@users.noreply.github.com>
2025-05-17 17:59:28 +0800

    Delete clean.sh

--- clean.sh
+++
@@ -1,3 +0,0 @@
-DIR="./"
-
-find "$DIR" -type f \( -name "*.aux" -o -name "*.fdb_latexmk" -o -name "*.fls" -o -name "*.log" -o -name "*.gz" -o -name "*.nav" -o -name "*.out" -o -name "*.vrb" -o -name "*.toc" -o -name "*.snm" -o -name "*.xml" -o -name "*.blg" -o -name "*.bcf" -o -name "*.bbl" -o -name "*.bak" -o -name "*Notes.bib" \) -exec rm -f {} \;
